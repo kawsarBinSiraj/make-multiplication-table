af9f1a4b2767d0ffce53806efb21d6ab3643a5f7
Kawsar Bin Siraj <38612699+kawsarBinSiraj@users.noreply.github.com>
2023-03-18 20:55:04 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+
+
+/**
+ * description :- calculate multiplication table
+ * created_at:- 18/03/2023 20:52:29
+ */
+function calculate(data) {
+    for (let d = 1; d <= 10; d++) {
+        let result = d + 'x' + data + '=' + d * data + '<br/>'
+        document.getElementById('output').insertAdjacentHTML('beforeend', result)
+    }
+}
+
+/**
+ * description :- click event for making the result
+ * created_at:- 18/03/2023 20:52:11
+ */
+document.getElementById('btn').addEventListener('click', function () {
+    let input = document.getElementById('input').value;
+    if (input == '') return alert('Input can not be empty !!')
+    calculate(input);
+});
+
+
+/**
+ * description :- reset
+ * created_at:- 18/03/2023 20:52:23
+ */
+document.getElementById('btn-reset').addEventListener('click', function () {
+    document.getElementById('input').value = '';
+    document.getElementById('output').innerHTML = ''
+});
+
+
